4c017fd05c1a0758f2f7a86c3375453710a1b25c
maurilio-vilela <mauriliovilela20@gmail.com>
2026-02-18 16:40:56 -0300

    fix(frontend): Correct path for globals.css import

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
+import './styles/globals.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 // 1. Crie uma instância do QueryClient
