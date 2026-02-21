bb54e7aba1c434a8e341925d508c7d757682e7cf
maurilio-vilela <mauriliovilela20@gmail.com>
2026-02-21 14:04:41 -0300

    fix: register whatsapp controller in channels module

--- backend/src/modules/channels/channels.module.ts
+++ backend/src/modules/channels/channels.module.ts
@@ -3,12 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ChannelsController } from './channels.controller';
 import { ChannelsService } from './channels.service';
 import { Channel } from './entities/channel.entity';
-import { WhatsAppModule } from './whatsapp/whatsapp.module';
+import { WhatsAppController } from './whatsapp/whatsapp.controller';
+import { WhatsAppService } from './whatsapp/whatsapp.service';
+import { WhatsAppSession } from './whatsapp/entities/whatsapp-session.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Channel]), WhatsAppModule],
-  controllers: [ChannelsController],
-  providers: [ChannelsService],
-  exports: [ChannelsService],
+  imports: [TypeOrmModule.forFeature([Channel, WhatsAppSession])],
+  controllers: [ChannelsController, WhatsAppController],
+  providers: [ChannelsService, WhatsAppService],
+  exports: [ChannelsService, WhatsAppService],
 })
 export class ChannelsModule {}
